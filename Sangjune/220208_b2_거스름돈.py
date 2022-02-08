d3a9e8306435ed7aff4b193de5d3883680d1f8bd
JJukE <psj9156@naver.com>
2022-02-08 17:32:03 +0900

    거스름돈 complete

--- Sangjune/220208_b2_거스름돈.py
+++ Sangjune/220208_b2_거스름돈.py
@@ -1,5 +1,16 @@
 import sys
 
-a = sys.stdin.readline()
+pay = int(sys.stdin.readline())
 
-print(a)
+n = 1000 - pay
+
+money = [500, 100, 50, 10, 5, 1]
+count = 0
+
+for i in money:
+    while n >= i:
+        n -= i
+        count += 1
+        if n == 0: break
+
+print(count)
