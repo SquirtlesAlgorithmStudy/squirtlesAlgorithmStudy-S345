a51bcc61ed1af96a29526aaf23e57880b5041d1b
laduck <64851790+laduck@users.noreply.github.com>
2022-02-12 10:28:35 +0000

    a

---
+++ 민석/그리디 알고리즘/2839-설탕배달.py
@@ -0,0 +1,26 @@
+need = int(input())
+a = 0
+b = 0
+k = [5, 3]
+
+for sugar in k:
+
+    if need % 3 == 0:
+        a = need // 3
+    elif need % sugar == 0:
+        b = b + need // sugar
+        need = need % sugar
+        if a != 0 and a > b:
+            print(b)
+        else:
+            print(a)
+    else:
+        print(-1)
+
+
+
+
+#print(a)
+#[a for sugar in k if need % sugar == 0 ]
+
+#[a if need % sugar == 0 else '-1' for sugar in k]
