64e2328b1e2502f5cf2918bc4c6aaede98058968
JJukE <psj9156@naver.com>
2022-02-12 15:48:23 +0900

    'sum' varname fixed

--- Sangjune/Greedy Algorithm/220211_s5_수들의합.py
+++ Sangjune/Greedy Algorithm/220211_s5_수들의합.py
@@ -2,11 +2,11 @@ import sys
 
 s = int(sys.stdin.readline())
 num = 1
-sum = 0
+S = 0
 
 for i in range(s):
-    sum += num
+    S += num
     num += 1
-    if s < sum:
+    if s < S:
         print(i)
         break
