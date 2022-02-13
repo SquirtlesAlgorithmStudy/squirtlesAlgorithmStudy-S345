de72d1117586abb7394b024a38ff518401e2c7a5
ChaMin <89429752+teaminii@users.noreply.github.com>
2022-02-13 14:14:03 +0000

    1543:문서검색

---
+++ main.py
@@ -0,0 +1,7 @@
+#  while True:
+#   document = list(map(str, input().split()))
+#   if document
+
+document = input()
+word = input()
+print(document.count(word))
---
+++ 차민/그리디/문서검색.py
@@ -0,0 +1,3 @@
+document = input()
+word = input()
+print(document.count(word))
