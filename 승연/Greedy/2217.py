94c7a8ffca3c718509335b565741c22ecd009e4e
seungg0 <61819350+seungg-0@users.noreply.github.com>
2022-02-19 05:27:41 +0000

    .

--- 승연/Greedy/2217.py
+++ 승연/Greedy/2217.py
@@ -1,30 +1,6 @@
 import sys
 
 data = []
-new_data = {}
-value_list = []
-key_list = []
-
-num = int(input())
-for _ in range(num):
-    data.append(int(sys.stdin.readline()))
-after = 0
-
-data.sort(reverse = True)
-
-for i in range(len(data)):
-    new_data[i] = data[i] * (i + 1)
-
-for value in new_data.values():
-    value_list.append(value)
-
-print(max(value_list))
-
-
-#더 짧게 바꿔보기
-import sys
-
-data = []
 
 num = int(input())
 for _ in range(num):
