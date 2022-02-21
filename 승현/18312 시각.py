2ded9c71564e766eeb1fc94ded45f1abebec5f65
Klaxon9718 <99737112+Klaxon9718@users.noreply.github.com>
2022-02-21 04:55:44 +0000

    fix

--- 승현/18312 시각.py
+++ 승현/18312 시각.py
@@ -1,5 +1,5 @@
-n = int(input())
-k = str(input())
+n,k = map(int, input().split())
+
 
 cnt = 0
 for i in range(n+1):
@@ -11,28 +11,3 @@ for i in range(n+1):
         if k in i.zfill(2)+j.zfill(2)+p.zfill(2):
           cnt += 1
 print(cnt)
-
-
-
-n,k = map(int, input().split())
-
-count = 0
-for i in range(n+1):
-  if i < 10:
-    pi = '0' +str(i)
-  else:
-    pi = str(i)
-  for j in range(60):
-    if j <10:
-      pj = '0'+str(j)
-    else :
-      pj = str(j)
-    for t in range(60):
-      if t < 10:
-        pt = '0'+str(t)
-      else:
-        pt = str(t)
-      if str(k) in pi + pj + pt:
-        count += 1
-
-print(count)
