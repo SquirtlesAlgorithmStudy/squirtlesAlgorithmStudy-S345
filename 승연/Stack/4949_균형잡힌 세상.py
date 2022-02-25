5eae3af58bfe228594fbb7bd298b3267421c8f5a
seungg0 <61819350+seungg-0@users.noreply.github.com>
2022-02-25 13:41:45 +0000

    4949수정

--- 승연/Stack/4949_균형잡힌 세상.py
+++ 승연/Stack/4949_균형잡힌 세상.py
@@ -1,31 +1,25 @@
-#왜안될까
-
 import sys
-# or line[i] == '[' or line[i] == ']':
+
 stack = []
 while True:
     line = sys.stdin.readline().rstrip() ##복습
     if line == '.':
         sys.exit()
     else:
-        # print(line)
         for i in range(len(line)):
             if (line[i] == '(') or (line[i] == '['):
                 stack.append(line[i])
-                # print(stack)
                 continue
             elif (line[i] == ')') or (line[i] == ']'):
                 if len(stack) > 0:
                     if stack[-1] == '(':
                         if line[i] == ')':
                             stack.pop(-1)
-                            # print(stack)
                         else:
                             stack.append(line[i])
                     elif stack[-1] == '[':
                         if line[i] == ']':
                             stack.pop(-1)
-                            # print(stack)
                         else:
                             stack.append(line[i])
                     else:
@@ -38,6 +32,4 @@ while True:
             print('yes')
         else:
             print('no')
-        # print('finish')
-# ([ (([( [ ] ) ( ) (( ))] )) ]).
-#  .
+        stack = []
