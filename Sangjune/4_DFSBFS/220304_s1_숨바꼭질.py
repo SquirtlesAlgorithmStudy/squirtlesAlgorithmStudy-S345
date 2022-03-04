9a532dcf748265db69f93db37fd01d1d11b81f28
JJukE <psj9156@naver.com>
2022-03-05 01:07:24 +0900

    숨바꼭질 solved!

---
+++ Sangjune/4_DFSBFS/220304_s1_숨바꼭질.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+
+fin = sys.stdin.readline
+
+def bfs(x, k):
+    arr = [0] * 10000000
+    if x == k:
+        return 0
+    queue = deque()
+    queue.append(x)
+
+    while queue:
+        x = queue.popleft()
+        for i in range(3):
+            if i == 2:
+                nx = 2 * x
+            else:
+                nx = x + dx[i]
+            if nx < 0 or nx > 200000:
+                continue
+            if arr[nx] == 0:
+                arr[nx] = arr[x] + 1
+                queue.append(nx)
+        if arr[k] != 0:
+            return arr[k]
+
+
+N, K = map(int, fin().rstrip().split())
+dx = [-1, 1, 0]
+
+print(bfs(N, K))
