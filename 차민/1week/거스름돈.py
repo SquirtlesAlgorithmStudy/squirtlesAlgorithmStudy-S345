0169dacc7a368d8ddebcc33379b70615e0591657
Pabby <leeuj9663@naver.com>
2022-03-06 15:16:05 +0900

    오류 해결

--- 차민/1week/거스름돈.py
+++
@@ -1,9 +0,0 @@
-change = 1000 - int(input())
-result = 0
-coin = [500, 100, 50, 10, 5, 1]
-
-for i in coin:
-  result += change // i
-  change %=i
-
-print(result)
--- 차민/1week/문서검색.py
+++
@@ -1,5 +0,0 @@
-document = input()
-word = input()
-print(document.count(word))
-
-#질문 ? 글자 수 제한
