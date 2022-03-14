c9453212d36ea507f44acf0e3a46e114ec00e3a2
0nnya <lmy1206119@gmail.com>
2022-03-14 17:17:28 +0900

    미완

--- 민영/DFS BFS 알고리즘/[1303] 전쟁 전투.py
+++ 민영/DFS BFS 알고리즘/[1303] 전쟁 전투.py
@@ -2,7 +2,47 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().strip().split())
+list1 = []
 
+data = [list(input().strip()) for _ in range(m)] #한 글자씩 저장
+# print (data)
 
-data = [list(map(int, input().strip().split())) for _ in range(m)]
-print (data)
+def dfs_W(x, y):
+    if x < 0 or x >= m:
+        return False
+    if visited[y][x] == 0 and data[y][x] == 'W':
+        visited[y][x] = 1
+        dfs_W(x-1, y)
+        dfs_W(x+1, y)
+        dfs_W(x, y-1)
+        dfs_W(x, y+1)
+        return True
+    return False
+
+def dfs_B(x, y):
+    if y < 0 or y >= n:
+        return False
+    if visited[y][x] == 0 and data[y][x] == 'B':
+        visited[y][x] = 1
+        dfs_B(x-1, y)
+        dfs_B(x+1, y)
+        dfs_B(x, y-1)
+        dfs_B(x, y+1)
+        return True    
+    return False
+
+countW = 0
+countB = 0
+visited = [[0] * m for _ in range(n)]
+for i in range(m):
+    for j in range(n):
+        if dfs_W(i , j) == True:
+            countW += 1    
+            
+visited = [[0] * m for _ in range(n)]
+for i in range(m):
+    for j in range(n):
+        if dfs_B(i , j) == True:
+            countB += 1
+
+print(countW*countW, countB*countB)
