b6ad623f65add29b7ff5db51d4776326f43e6bcb
0nnya <lmy1206119@gmail.com>
2022-03-14 15:18:23 +0900

    미완

---
+++ 민영/DFS BFS 알고리즘/[1303] 전쟁 전투.py
@@ -0,0 +1,8 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().strip().split())
+
+
+data = [list(map(int, input().strip().split())) for _ in range(m)]
+print (data)
