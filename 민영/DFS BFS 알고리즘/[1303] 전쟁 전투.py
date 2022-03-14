2c35105937b0bd54db0437824d6ed6c83dd232ac
0nnya <lmy1206119@gmail.com>
2022-03-14 17:37:30 +0900

    미완,,,ㅠ

--- 민영/DFS BFS 알고리즘/[1303] 전쟁 전투.py
+++ 민영/DFS BFS 알고리즘/[1303] 전쟁 전투.py
@@ -1,4 +1,5 @@
 from collections import deque
+from itertools import count
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().strip().split())
@@ -7,42 +8,49 @@ list1 = []
 data = [list(input().strip()) for _ in range(m)] #한 글자씩 저장
 # print (data)
 
-def dfs_W(x, y):
-    if x < 0 or x >= m:
-        return False
+def dfs_W(x, y, cnt):
+    if(y < 0 or y >= n) & (x < 0 or x >= m):
+        return 0
     if visited[y][x] == 0 and data[y][x] == 'W':
         visited[y][x] = 1
-        dfs_W(x-1, y)
-        dfs_W(x+1, y)
-        dfs_W(x, y-1)
-        dfs_W(x, y+1)
-        return True
-    return False
+        cnt += 1
+        dfs_W(x-1, y, cnt)
+        dfs_W(x+1, y, cnt)
+        dfs_W(x, y-1, cnt)
+        dfs_W(x, y+1, cnt)
+        return cnt
+    return 0
 
-def dfs_B(x, y):
-    if y < 0 or y >= n:
-        return False
+def dfs_B(x, y, cnt):
+    if (y < 0 or y >= n) & (x < 0 or x >= m):
+        return 0
     if visited[y][x] == 0 and data[y][x] == 'B':
         visited[y][x] = 1
-        dfs_B(x-1, y)
-        dfs_B(x+1, y)
-        dfs_B(x, y-1)
-        dfs_B(x, y+1)
-        return True    
-    return False
+        cnt += 1
+        dfs_B(x-1, y, cnt)
+        dfs_B(x+1, y, cnt)
+        dfs_B(x, y-1, cnt)
+        dfs_B(x, y+1, cnt)
+        return cnt  
+    return 0
 
-countW = 0
-countB = 0
+countW = []
+countB = []
 visited = [[0] * m for _ in range(n)]
 for i in range(m):
     for j in range(n):
-        if dfs_W(i , j) == True:
-            countW += 1    
-            
+        if dfs_W(i , j, 0) != 0:
+            countW.append(dfs_W(i , j, 0))    
+
 visited = [[0] * m for _ in range(n)]
 for i in range(m):
     for j in range(n):
-        if dfs_B(i , j) == True:
-            countB += 1
-
-print(countW*countW, countB*countB)
+        if dfs_B(i , j, 0) != 0:
+            countB.append(dfs_B(i , j, 0))
+cntW = 0
+cntB = 0
+for i in countW:
+    cntW += i**2
+for i in countB:
+    cntB += i**2
+print(cntW, cntB)
