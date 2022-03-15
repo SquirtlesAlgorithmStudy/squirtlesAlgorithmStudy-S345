8ccafd9c77570304f3ad3f27bfb6f15c76733395
ChaMin <89429752+teaminii@users.noreply.github.com>
2022-03-15 14:58:50 +0000

    1303 문제를 똑바로 읽자

--- 차민/DFSBFS/1303전쟁.py
+++ 차민/DFSBFS/1303전쟁.py
@@ -1,4 +1,5 @@
-n,m = map(int,input().split())
+#입력받기 
+m,n = map(int,input().split())
 graph = []
 color = []
 
@@ -7,9 +8,9 @@ for i in range(n):
 
 def bfs_white(x,y):
   if x<=-1 or x>=n or y<=-1 or y>=m:
-    return 0
+    return
   if graph[x][y] == 'W':
-    graph[x][y]=1
+    graph[x][y]= 1
     color.append(graph[x][y])
 
     bfs_white(x-1,y)
@@ -17,22 +18,22 @@ def bfs_white(x,y):
     bfs_white(x+1,y)
     bfs_white(x,y+1)
 
-  return 0
+  return 
 
 
 def bfs_blue(x,y):
   if x<=-1 or x>=n or y<=-1 or y>=m:
-    return 0
+    return 
   if graph[x][y] == 'B':
-    graph[x][y]=-1
+    graph[x][y]= -1
     color.append(graph[x][y])
 
     bfs_blue(x-1,y)
     bfs_blue(x,y-1)
     bfs_blue(x+1,y)
     bfs_blue(x,y+1)
-
-  return 0
+    
+  return 
 
 
 for i in range(n):
@@ -52,8 +53,6 @@ sum_white = 0
 sum_blue = 0
 
 
-
-
 #최대값(모든 성분이 1, -1로 교차될 때)을 range로
 for k in range(length-1,-1,-1):
 
@@ -64,7 +63,7 @@ for k in range(length-1,-1,-1):
       sum_white += sum**2
     else :
       sum_blue += sum**2
-
+      
     break
     
   
