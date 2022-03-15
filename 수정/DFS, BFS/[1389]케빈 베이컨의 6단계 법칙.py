ec0d5ae0c958fae12e311ced561337e07445a68b
Woo Soojeong <kalgus76@naver.com>
2022-03-16 01:26:53 +0900

    BFS

--- 수정/DFS, BFS/[1389]케빈 베이컨의 6단계 법칙.py
+++ 수정/DFS, BFS/[1389]케빈 베이컨의 6단계 법칙.py
@@ -3,27 +3,27 @@ import sys
 fastin = sys.stdin.readline
 
 User, Relation = map(int, fastin().rstrip().split())
-R = deque([[0] * (User + 1) for _ in range(Relation + 1)])
+R = deque([[0] * 1 for _ in range(User + 1)])
 for i in range(Relation):
     A, B = map(int, fastin().rstrip().split())
     R[A].append(B)
     R[B].append(A)
-for j in range(1, Relation + 1):
-    for k in range(User + 1):
-        R[j].pop(0)
+for j in range(1, User + 1):
+    R[j].pop(0)
 # print(R)
 
 def bfs(R, num):
     queue = deque([num])
     visit = [0] * (User + 1)
+    visit[num] = 1
     while queue:
         num = queue.popleft()
         for i in R[num]:
             if visit[i] == 0:
                 visit[i] = visit[num] + 1
                 queue.append(i)
-    return sum(visit)
-#print(bfs(R, 1))
+    return sum(visit) - User
+# print(bfs(R, 1))
 
 Sum = bfs(R, 1)
 mini = 1
