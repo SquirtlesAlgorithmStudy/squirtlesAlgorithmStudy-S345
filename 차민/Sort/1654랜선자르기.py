7ada6dd7a634f4e7ba37e35d112e844f99478617
ChaMin <89429752+teaminii@users.noreply.github.com>
2022-03-22 13:47:37 +0000

    1654

--- 차민/Sort/1654랜선자르기.py
+++ 차민/Sort/1654랜선자르기.py
@@ -1,16 +1,19 @@
 k,n= map(int, input().split())
 line = []
+
 for _ in range(k):
     line.append(int(input()))
 start = 1
 end = max(line)
- 
+
+
+
 while(start<=end):
     mid = (start+end)//2
     cnt = 0
     for i in line:
         cnt += i//mid
-    if cnt>=n: #자른 개수가 많으면 -> 더 크게잘라야함
+    if cnt>=n: 
         start = mid+1
     else:
         end = mid-1
