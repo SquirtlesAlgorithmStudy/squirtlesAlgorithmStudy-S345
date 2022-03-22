d124f93cd005e4f211930f9a0c0a8876418d4c45
ChaMin <89429752+teaminii@users.noreply.github.com>
2022-03-22 13:48:26 +0000

    10825

--- 차민/Sort/10825국영수.py
+++ 차민/Sort/10825국영수.py
@@ -1,17 +1,10 @@
-k,n= map(int, input().split())
-line = []
-for _ in range(k):
-    line.append(int(input()))
-start = 1
-end = max(line)
- 
-while(start<=end):
-    mid = (start+end)//2
-    cnt = 0
-    for i in line:
-        cnt += i//mid
-    if cnt>=n: #자른 개수가 많으면 -> 더 크게잘라야함
-        start = mid+1
-    else:
-        end = mid-1
-print(end)
+n = int(input())
+
+a = []
+for _ in range(n):
+    name, korean, english, math = input().split()
+    a.append((name, int(korean), int(english), int(math)))
+
+
+
+for i in sorted(a, key = lambda x : (-x[1], x[2], -x[3], x[0])):
