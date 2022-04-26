d655fd821a712a225c17669508ecb6667222d722
ChaMin <89429752+teaminii@users.noreply.github.com>
2022-04-26 10:41:29 +0000

    DP 2434

--- main.py
+++ main.py
@@ -1,31 +1,4 @@
-d = [0]*100
 
 
-def fibo(x):
-  if x==0 :
-    cnt[0] = cnt[0]+1
 
-    return 0
-  if x==1 :
-    cnt[1] = cnt[1]+1
 
-    return 1
-
-  d[x] = fibo(x-1)+fibo(x-2)
-  
-  return [cnt[0], cnt[1]]
-
-tc = int(input())
-x = []
-
-for i in range(tc):
-  x.append(int(input()))
-
-a=0
-b=0
-for j in x:
-  cnt = [0]*2
-  a,b = fibo(j)
-
-  print(a,b)
-  
