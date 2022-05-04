9834f0c23a1fa3714882b080a1611de5e6be60d8
ChaMin <89429752+teaminii@users.noreply.github.com>
2022-05-04 08:36:43 +0000

    계단오르기

--- 차민/DP/2579.py
+++ 차민/DP/2579.py
@@ -8,9 +8,11 @@ for i in range(n):
 d = [0]*100
 
 d[1] = score[1]
-d[2] = score[1]+score[2]
 
-for i in range(3,n+1):
-  d[i] = max(d[i-3]+score[i]+score[i-1], d[i-2]+score[i])
+if n>1:
+  d[2] = score[1]+score[2]
 
-print(d[i])
+  for i in range(3,n+1):
+    d[i] = max(d[i-3]+score[i]+score[i-1], d[i-2])
+
+  print(d[i])
