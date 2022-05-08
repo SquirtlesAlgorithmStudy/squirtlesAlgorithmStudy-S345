475c577f04ac54215105744fefb5ee2c900b2e39
소영 <sso_0022@naver.com>
2022-05-09 01:14:04 +0900

    [소영] 백준 11047 동전 0 설명 추가

--- 소영/Baekjoon/Baekjoon/그리디/11047_동전 0.swift
+++ 소영/Baekjoon/Baekjoon/그리디/11047_동전 0.swift
@@ -9,9 +9,13 @@ func 동전_0() {
   let read = readLine()!.split(separator: " ").map{ Int($0)! }
   let coinsCount = read[0]
   var cost = read[1]
+  // 큰 동전부터 체크하기 위해 reversed 시켜서 저장
   let coins: [Int] = (0..<coinsCount).map { _ in Int(readLine()!)! }.reversed()
   var count: Int = 0
   
+  // 동전들을 순회하면서 남아있는 가치 내에서
+  // 해당 동전의 개수를 최대한 많이 빼낼 수 있도록 한다. (큰 동전부터 순회함)
+  // 남은 가치가 0이 되면 break
   for coin in coins {
     let howManyCoins = cost / coin
     
