a5a5426ad06f7fac0134778d4b67ee3d1d1451ff
Yujin Lee <yj587878@gmail.com>
2022-05-09 10:10:20 +0900

    [1주차] 1789번 파일 누락 추가

--- 이유진/그리디/boj_1789.swift
+++ 이유진/그리디/boj_1789.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+//12ms,79508KB
+
+if let input = readLine() {
+    var S = Int(input)!
+    var sum = 0
+    var i = 0
+    var result = 0
+    
+    while(S >= sum){
+        i+=1
+        sum+=i
+        
+    }
+    result = i-1
+  print(result)
+}
