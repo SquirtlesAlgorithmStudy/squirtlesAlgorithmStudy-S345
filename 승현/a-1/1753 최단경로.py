ea9053e63d5615cccc2fa11f203d5ace7ba16fd5
Klaxon9718 <99737112+Klaxon9718@users.noreply.github.com>
2022-05-10 15:01:14 +0000

    1753 최단경로

---
+++ 승현/a-1/1753 최단경로.py
@@ -0,0 +1,35 @@
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start =  int(input())
+graph = [[] for i in range (n +1)]
+distance =  [ INF ] * (  n + 1)
+
+for _ in range(m):
+  a,b,c = map(int, input().split())
+  graph[a].append((b,c))
+
+def dijkstra (start):
+  q = []
+  heapq.heappush(q, (0,start))
+  distance[start] = 0
+  while q:
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+      continue
+    for i in graph[now]:
+      cost = dist +i[1]
+      if cost < distance[i[0]]:
+        distance[i[0]] = cost
+        heapq.heappush(q,(cost,i[0]))
+
+dijkstra(start)
+
+for i in range(1, n+1):
+  if distance[i] == INF:
+    print("INF")
+  else:
+    print(distance[i])
