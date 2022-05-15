1a0f8ce485dea028bcc313fe5032d6569ea85a3a
Yujin Lee <yj587878@gmail.com>
2022-05-15 23:57:10 +0900

    [2주차] boj_18312 누락 파일 추가

--- 이유진/구현/boj_18312.swift
+++ 이유진/구현/boj_18312.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+var N = Int()
+var K = Int()
+if let input = readLine() {
+    let arrInput = input.components(separatedBy: " ")
+    N = Int(arrInput[0])!
+    K = Int(arrInput[1])!
+}
+var count = 0
+for h in 0...N{
+    for m in 0...59{
+        for s in 0...59{
+            if (h/10 == K || h%10 == K ||
+                m/10 == K || m%10 == K ||
+                s/10 == K || s%10 == K){
+                count+=1
+            }else{
+                continue
+            }
+        }
+    }
+}
+
+print(count)
