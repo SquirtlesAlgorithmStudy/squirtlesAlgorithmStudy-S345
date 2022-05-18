3e57b85f0031b81a3f50f148407a28d3ed57c6b8
Klaxon9718 <99737112+Klaxon9718@users.noreply.github.com>
2022-05-18 12:06:01 +0000

    16234 수정

--- 승현/a-2/16234 인구이동.py
+++ 승현/a-2/16234 인구이동.py
@@ -1,46 +1,50 @@
-
 #16234 인구이동
 from collections import deque
 import sys
 input = sys.stdin.readline
 
 def BFS(x, y):
-  graph = []
+  temp = []
   queue = deque()
   queue.append((0,0))
+  temp.append([x,y])
 
   while queue:
     x, y = queue.popleft()
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
-
-      if nx < 0 or nx >= line or ny < 0 or ny >= line:
-        continue
-      if graph[nx][ny]: #
-        continue
-      
-      if abs(array[x][y] - array[nx][ny]) < over and abs(array[x][y] - array[nx][ny]) > under:
-        queue.append((nx,ny))
-        graph[x][y] = True
-        graph[nx][ny] = True
-        cnt += 1
-        
-      if graph[nx][ny] == 1:
-        graph[nx][ny] = graph[x][y] + 1
-        queue.append((nx,ny))
-               
-
-
-
+      if 0 <= nx < line and 0 <= ny < line and visit[nx][ny] == 0:
+        if under <= abs(s[nx][ny] - s[x][y]) <= over:
+          visit[nx][ny] = 1
+          queue.append([nx,ny])
+          temp.append([nx,ny])
+  return temp
+  
 ##Main##
 #칸 수, 최소값, 최댓값
 line, under, over = map(int, input().split())
-array = []
-
+s = []
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-for _ in range(line):
-  array.append(input().split())
-  
-#union[(i,j)]
+s = [list(map(int, input()))for _ in range(line)]
+cnt = 0
+while True:
+  visit = [[0] * line for i in range(line)]
+  isTrue = False
+  for i in range(line):
+    for j in range(line):
+      if visit[i][j] == 0:
+        visit[i][j] = 1
+        temp = BFS(i,j)
+        if len(temp) > 1:
+          isTrue = True
+          num = sum([s[x][y]for x,y in temp]) //len(temp)
+          for x, y in temp:
+            s[x][y] = num
+  if not isTrue:
+    break
+  cnt += 1
+print(cnt)
+
+#https://pacific-ocean.tistory.com/375
