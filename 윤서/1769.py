8abaa16382467cc233f1123c25074b20828a50fd
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-06-02 01:53:42 +0900

    [BOJ] 1769_3의 배수

--- 윤서/1769.py
+++ 윤서/1769.py
@@ -1,23 +1,30 @@
-# 뇌피셜 코드,,
 # 1769 - 3의 배수
 
 import sys
 input = sys.stdin.readline
 
-x = input()
+number = input().rstrip()
 count = 0
 
-def checkNum(num):
-    y = 0
+def checkThree(number):                     # type(number) == str
     global count
-    count += 1
-    for i in num:
-        y += i                      # 오류 수정 전
-    if num != y:
-        checkNum(y)
-    else:
-        if y % 3 == 0:
-            print("yes")
-            return count
+    if len(number) != 1:                    # 숫자가 한 자리 수가 아닐 때
+        count+=1                            # 계산 수행
+    checkNum = 0                
 
-checkNum(x)
+    for i in str(number):
+        checkNum += int(i)                  # 각 자리의 숫자의 합
+
+    if len(str(checkNum)) != 1:             # 문자열의 길이가 1(정수 자릿수가 1)이 아니라면: int형은 len함수 사용할 수 없음
+        checkThree(str(checkNum))           # checkThree함수가 number를 str로 받기 때문에 형 변환 필수!
+    elif len(str(checkNum)) == 1:           # 정수 자릿수가 1이라면
+        if checkNum % 3 != 0:               # 3의 배수인지 아닌지 확인
+            print(count)
+            print("NO")
+            return
+        elif checkNum % 3 == 0:
+            print(count)
+            print("YES")
+            return
+
+checkThree(number)
