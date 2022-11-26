99a51678d46d926c182e053f2dda668a0e34b1ef
HyeBeen <ohhyebin147@naver.com>
2022-11-26 18:29:22 +0900

    힘들다

--- 혜빈/피본나치.py
+++ 혜빈/피본나치.py
@@ -8,3 +8,18 @@ numbers = []
 
 for _ in range(n):
   numbers.append(int( input().rstrip()))
+
+fibo=[]
+fibo.append(0)
+fibo.append(1)
+
+for number in numbers:
+  while fibo[-1]<=number-fibo[-2] :
+    fibo.append(fibo[-1]+fibo[-2])
+  print(fibo)
+    
+    
+  
+ 
+
+           
