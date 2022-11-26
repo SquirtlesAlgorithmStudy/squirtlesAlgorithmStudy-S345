af0069a8f2e933b13d0501a39c1b5540ec9ac317
SeongYong <pjii50260@naver.com>
2022-11-26 17:22:46 +0900

    피보나치

--- SeongYong/greedy/temp.py
+++ SeongYong/greedy/temp.py
@@ -1,12 +1,42 @@
-prob = input().split('-')
+import sys; input = sys.stdin.readline
 
-sum = 0
+N = int(input())
+prob = []
+for _ in range(N):
+    prob.append(int(input()))
 
-for i in prob[1:]:
-    for j in i.split('+'):
-        sum -=int(j)
+itter_val = max(prob)
+
+li = [1, 1]
+for i in range(itter_val):
+    li.insert(i+2, li[i]+li[i+1])
+    if li[i+2] > itter_val: break
+
+i = 1
+ans = []
+
+for i in prob:
+    ans = []
+    for j in li[::-1]:
+        if i - j >= 0:
+            ans.append(j)
+            i = i - j
+        if i == 0: 
+            for k in range(len(ans)-1, -1, -1):
+                print(ans[k], end =' ')
+            print()
+            break
+
         
-sum += int(prob[0].split('-'))
-print(sum)
+        
+    
+    
+
+
+
+
+
+
+
 
 
