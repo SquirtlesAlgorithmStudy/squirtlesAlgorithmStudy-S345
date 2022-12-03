6913e105f2a3954953a7a2f80175a48f45da0fa8
SangminByeon <bsm7302@naver.com>
2022-12-03 21:15:52 +0900

    물병 푼데까지 정리

--- SangminByeon/1052_물병.py
+++ SangminByeon/1052_물병.py
@@ -3,22 +3,20 @@ from sys import stdin
 n, k = map(int, stdin.readline().rstrip().split())
 
 l = 1 
-base = (n, l) # n개의 물병, 1리터의 물
+#base = (n, l) # n개의 물병, 1리터의 물
 
 n_new = n
 l_new = l
 shop = 0
 
-while True:
+while n_new == k: #n과 k가 같아질 때 까지 반복
     cnt = shop
-    while True: #자연수로 나누어지는데 까지 일단 물병 합침
+    while n_new/2 == n_new//2: #자연수로 나누어지는데 까지 일단 물병 합침
         n_new = n_new/2
         l_new = l_new*2
         cnt += 1
-        if n_new/2 == n_new//2: break
 
-    if n_new == k: 
-        
+    shop += cnt        
 
 print(shop)
 
