6988bb5c87928d39ef63f551fc4ce98e043eb79a
Yeongjin <njun136@naver.com>
2022-12-23 16:52:51 +0900

    물 못 막는 항승이

--- 영진/그리디/1449_수리공항승.py
+++ 영진/그리디/1449_수리공항승.py
@@ -1,42 +1,61 @@
-#물이 새는 파이프에 테이를 붙이는데 물이 새는 지점의 좌우 0.5씩은 간격을 주어야 함
-#첫째 줄에 물이 새는 곳의 개수 N과 테이프의 길이 L이 주어진다. 
-#둘째 줄에는 물이 새는 곳의 위치가 주어진다. N과 L은 1,000보다 작거나 같은 자연수이고, 물이 새는 곳의 위치는 1,000보다 작거나 같은 자연수이다.
-#첫째 줄에 항승이가 필요한 테이프의 개수를 출력한다.
+## 가장 왼쪽에서 정무만큼 떨어진 거리만 물이 샐 때,
+## 테이프를 이용하여 물이 새는 곳을 기준으로 좌우 0.5만큼 간격울 주어 막음
+## 물이 새는 위치(물이 새는 곳의 개수N), 테이프의 길이(L)가 주어짐
+## 필요한 최소한의 테이프의 수?(N,L<=1000인 자연수)
 
 """
-<1>-2
+<분석>
+
+(1)
 4 2
 1 2 100 101
+->2
 
-<2>-2
+=> 물이 새는 곳마다 최소 1만큼의 테이프를 사용
+   연속한 물의 새는 곳의 개수가 테이프의 길이 보다 작거나 같을 경우,
+   하나의 테이프로 막을 수 있음
+
+(2)
 4 3
 1 2 3 4
+->2
 
-<3>-3
+=> 물이 새는 곳마다 최소 1만큼의 테이프를 사용
+   연속한 물의 새는 곳의 개수가 테이프의 길이 보다 작거나 같을 경우,
+   하나의 테이프로 막을 수 있음
+
+
+(3)
 3 1
 3 2 1
+->3
 
-=>기준점으로부터 다른 지점까지의 차+1이 테이프의 길이보다 작거나 같을 때까지 동작
-  이후 다음 테이프 사용
-
+=>물이 새는 곳마다 최소 1만큼의 테이프를 사용
+   연속한 물의 새는 곳의 개수가 테이프의 길이 보다 작거나 같을 경우,
+   하나의 테이프로 막을 수 있음
+   
+<시도1>
+1.물이 새는 위치를 입력받음
+2.물이 새는 위치가 연속한지 확인
+3.연속한 위치의 수가 테이프의 길이보다 작거나 같은지 확인
+4(1).작거나 같을 경우, 같은 테이프 사용(테이프의 수 카운트x)
+4(2).작거나 같지 않을 경우, 테이프의 수 카운트
+5.카운트한 테이프의 수 출력
 """
-# 물이 새는 곳의 개수 N과 길이 L입력
-N,L=map(int,input().split())
 
-#물 새는 포인트 저장
-point=list(map(int, input().split()))
-point.sort(reverse=True)
+num_LeakingSpot,length_tape=map(int,input().split())
+leaking_spot=list(map(int, input().split()))
+leaking_spot.sort(reverse=False)
+     
+num_needed_tape=0
+needed_length_tape=1
+start_spot=leaking_spot[0]
 
-#필요한 테이프의 길이와 개수 
-needed_tape_num=0
-point_start=0
-point_finish=1
-
-while(point_finish!=len(point)):
-    if (point[point_start]-point_finish+1)>=L:
-        needed_tape_num+=1
-    elif (point[point_start]-point_finish+1)<L:
-        point_finish+=1
-    else:
+for i in range(len(leaking_spot)):
+    if leaking_spot[i]-start_spot+1<=length_tape:
         break
-print(needed_tape_num)
+    else:
+        start_spot=leaking_spot[i]
+        num_needed_tape+=1
+    num_needed_tape+=1
+print(num_needed_tape)
--- 영진/코딩테스트/5585_거스름돈.py
+++
@@ -1,7 +0,0 @@
-change = 1000 - int(input())
-coin= [500, 100, 50, 10, 5, 1]
-num_coin = 0
-for i in coin:
-    num_coin += change// i
-    change %= i
-print(num_coin)
