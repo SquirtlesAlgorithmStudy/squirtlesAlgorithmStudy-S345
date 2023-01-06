3e21dc17ae7d6fddb90bc5cdf129b24286862fb2
lee-euiji <leeuj9663@naver.com>
2023-01-06 16:07:29 +0900

    [의진] 슬라이싱과 반복문의 속도차이

---
+++ 의진/구현/2563 색종이 살짝 개선.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+colored_papers = [tuple(map(int, input().split())) for _ in range(n)]
+board = [[0] * 100 for _ in range(100)]
+
+for colored_paper in colored_papers:
+    for row in range(colored_paper[1], colored_paper[1] + 10):
+        board[row][colored_paper[0]: colored_paper[0] + 10] = [1] * 10
+
+count = 0
+for i in range(100):
+    for j in range(100):
+        if board[i][j] == 1:
+            count += 1
+
+print(count)
