c1ec121c141593562f6fda007fff2cb2422e3197
Yeongjin <njun136@naver.com>
2023-01-10 14:26:11 +0900

    균형잡힌 세상

---
+++ 영진/자료 구조/4949_균형잡힌 세상.py
@@ -0,0 +1,28 @@
+while True:
+    stack=[]
+    
+    script=input()
+    if script=='.':
+        break
+    
+    for ch in script:
+        if ch=='('or ch=='[':
+            stack.append(ch)
+        elif ch==')':
+            if len(stack)!=0 and stack[-1]=='(':
+                stack.pop()
+            else:
+                stack.append(ch)
+        elif ch==']':
+            if len(stack)!=0 and stack[-1]=='[':
+                stack.pop()
+            else:
+                stack.append(ch)
+
+    if len(stack)==0:
+        print("yes")
+    else:
+        print("no")
+
+            
+    
