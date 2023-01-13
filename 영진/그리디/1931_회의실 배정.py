11fb7ed41ba67bea606bdef26ee54b653c6c0d0d
Yeongjin <njun136@naver.com>
2023-01-13 11:13:57 +0900

    회의실 배정

---
+++ 영진/그리디/1931_회의실 배정.py
@@ -0,0 +1,19 @@
+N=int(input())
+
+from sys import stdin
+conference_time=[]
+for _ in range(N):
+    start_time,end_time=list(map(int,stdin.readline().rstrip().split()))
+    conference_time.append([start_time,end_time])
+    
+conference_time=sorted(conference_time,key=lambda x:(x[1],x[0]))
+
+num_of_conference=0
+last_end_time=0
+
+for start,end in conference_time:
+    if start>=last_end_time:
+        num_of_conference+=1
+        last_end_time=end
+
+print(num_of_conference)
