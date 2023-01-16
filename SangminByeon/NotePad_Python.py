d26d150a912f5466926ca3520931f7ed8ae86a8c
Trilly <tttrillyyy@gmail.com>
2023-01-16 21:50:22 +0900

    Softeer PS수학연산으로 차원 줄이기

--- SangminByeon/NotePad_Python.py
+++ SangminByeon/NotePad_Python.py
@@ -1,7 +1,16 @@
-str1 = ["____" for _ in range(2)]
-print(*str1)
+from sys import stdin
 
-str2 = ""
-for _ in range(3):
-    str2 = str2 + "____"
-print(str2)
+n = int(stdin.readline().rstrip())
+list = stdin.readline().strip().split()
+str = "".join(list)
+
+one = str.split('2')
+two = str.split('1')
+
+one_idx = one.index(max(one))
+two_idx = two.index(max(two))
+
+one_fin = len(one[one_idx])
+two_fin = len(two[two_idx])
+
+print(max(one_fin, two_fin))
