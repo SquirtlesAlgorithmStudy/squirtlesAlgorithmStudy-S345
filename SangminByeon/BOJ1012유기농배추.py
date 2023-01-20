9046971a9aef7aa00da3a829cb9914202014999b
Trilly <tttrillyyy@gmail.com>
2023-01-20 20:32:44 +0900

    바이러스 수정

--- SangminByeon/BOJ1012유기농배추.py
+++ SangminByeon/BOJ1012유기농배추.py
@@ -11,19 +11,19 @@ def dfs(x,y):
             visited[nx][ny] = 1
             dfs(nx, ny)
 
-    T = int(stdin.readline().rstrip())
-    for _ in range(T):
-        M, N, K = map(int,stdin.readline().split())
-        field = [[0] * M for _ in range(N)]
-        visited = [[0] * M for _ in range(N)]
-        cnt = 0
-        for _ in range(K):
-            a, b = map(int,stdin.readline().split())
-            field[b][a] = 1
-        for i in range(N):
-            for j in range(M):
-                if field[i][j] and not visited[i][j]:
-                    visited[i][j] = 1
-                    cnt += 1
-                    dfs(i,j)
-        print(cnt)
+T = int(stdin.readline().rstrip())
+for _ in range(T):
+    M, N, K = map(int,stdin.readline().split())
+    field = [[0] * M for _ in range(N)]
+    visited = [[0] * M for _ in range(N)]
+    cnt = 0
+    for _ in range(K):
+        a, b = map(int,stdin.readline().split())
+        field[b][a] = 1
+    for i in range(N):
+        for j in range(M):
+            if field[i][j] and not visited[i][j]:
+                visited[i][j] = 1
+                cnt += 1
+                dfs(i,j)
+    print(cnt)
