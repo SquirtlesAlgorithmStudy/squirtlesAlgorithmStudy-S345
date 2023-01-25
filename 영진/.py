664ff1598a59762921cb038d6c04035c58aa26c8
Yeongjin <njun136@naver.com>
2023-01-25 21:16:05 +0900

    1303

--- 영진/.py
+++ 영진/.py
@@ -1,14 +1,3 @@
-N, M = map(int, input().split())
-ans = []
-
-def back():
-    if len(ans) == M: # 배열의 길이를 확인
-        print(" ".join(map(str, ans))) # 1 2 3 이런 상태로 출력하기 위해
-        return 
-    for i in range(1, N+1): # 1 ~ N 까지 
-        if i not in ans: # 중복 확인  
-            ans.append(i) # 배열 추가
-            back() # 재귀
-            ans.pop() # return으로 돌아오면 이게 실행됨. 1, 2, 3 일때 3을 없앰으로 전 단계로 돌아가는 것
-            
-back()
+a=2
+a=a**3
+print(a)
---
+++ 영진/DFS BFS/1303_전쟁.py
@@ -0,0 +1,40 @@
+from collections import deque
+from sys import stdin
+N,M=map(int,stdin.readline().rstrip().split())
+
+war=[list(input()) for _ in range(M)]
+visited=[[False]*N for _ in range(M)]
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+def bfs(x,y,color):
+    count=1
+    queue=deque()
+    queue.append((x,y))
+    visited[x][y]=True
+
+    while queue:
+        x,y=queue.popleft()
+        
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            
+            if 0<=nx<M and 0<=ny<N:
+                if war[nx][ny]==color and not visited[nx][ny]:
+                    visited[nx][ny]=True
+                    queue.append((nx,ny))
+                    count+=1
+    
+    return count
+white=0
+blue=0
+for i in range(M):
+    for j in range(N):
+        if war[i][j]=='W' and not visited[i][j]:
+            white+=bfs(i,j,'W')**2
+        elif war[i][j]=='B' and not visited[i][j]:
+            blue+=bfs(i,j,'B')**2
+            
+            
+print(white,blue)                
