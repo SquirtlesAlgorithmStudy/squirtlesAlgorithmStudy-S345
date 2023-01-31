12555439ba8cd3bd5bf96308727604838d97e6ff
lee-euiji <leeuj9663@naver.com>
2023-01-31 16:06:20 +0900

    [의진] 12100 2048 Easy

--- 의진/DFS_BFS/15651 N과 M.py
+++ 의진/DFS_BFS/15651 N과 M.py
@@ -21,3 +21,22 @@ def dfs(depth):
 
 
 dfs(1)
+
+
+# 더 시간이 적게 걸리는 풀이
+
+input = __import__("sys").stdin.readline
+
+n, r = map(int, input().split())
+
+def dfs(arr):
+    if len(arr) == r:
+        print(*arr)
+        return 
+
+    for i in range(1, n+1):
+        arr.append(i)
+        dfs(arr)
+        arr.pop()
+
+dfs([])
