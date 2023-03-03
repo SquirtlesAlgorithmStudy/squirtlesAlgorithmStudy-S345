1f25be8f6b77570f79660e655725302eea1d9031
Yeongjin <njun136@naver.com>
2023-03-03 17:36:42 +0900

    /

--- 영진/다이나믹 프로그래밍/2156_포도주 시식.py
+++ 영진/다이나믹 프로그래밍/2156_포도주 시식.py
@@ -9,6 +9,7 @@ dp=[0]*n
 dp[0]=wine[0]
 dp[1]=wine[0]+wine[1]
 for i in range(2,n):
-    dp[i]=max(dp[i-3]+wine[i]+wine[i-1],dp[i-2]+wine[i],wine[i-1])
+    # i,i-1번째를 마시는 경우, i번째는 마시고 i-1번째는 안 마시는 경우, i번째를 안 마시는 경우
+    dp[i]=max(dp[i-3]+wine[i]+wine[i-1],dp[i-2]+wine[i],wine[i-1]) 
 
 print(max(dp))
