cf4e3d073a809bcb743ff54d4e48bc173e65694c
Yeongjin <njun136@naver.com>
2023-03-03 15:32:41 +0900

    .

---
+++ 영진/다이나믹 프로그래밍/14501_퇴사.py
@@ -0,0 +1,14 @@
+#특정 날까지의 수익을 계산할 때 이전에 저장했던 값을 이용해서 계산하므로 dp
+
+from sys import stdin
+N=int(input())
+T_P=[list(map(int,stdin.readline().rstrip().split())) for _ in range(N)]
+
+dp=[0]*(N+1)
+
+for i in range(N):
+    for j in range(i+T_P[i][0],N+1): #특정 일의 상담을 받았을 경우, 다음 상담을 할 수 있을 때부터 퇴사 전까지
+        if dp[j]<dp[i]+T_P[i][1]: #상담을 받을 수 있는 일정인 경우, 최대 수익을 창출하도록
+            dp[j]=dp[i]+T_P[i][1]
+            
+print(dp[-1]) #가장 마지막으로 업데이트한 dp값이 최대일 것이므로 index=-1
--- 영진/자료 구조/1406_에디터.py
+++ 영진/자료 구조/1406_에디터.py
@@ -21,4 +21,27 @@ print("".join(Left_list+list(reversed(Right_list))))
 """
 1. reversed() :리스트의 값을 좌우반전 시켜준다.
 2. 다중 if문보다 elif를 사용하는 것이 시간이 절약된다.
+
+<시간 초과>
+text=list(input())
+cursor=len(text)
+M=int(input())
+
+for _ in range(M):
+    command=input().split()
+    if command[0]=='L':
+        if cursor!=0:
+            cursor-=1
+    elif command[0]=='D':
+        if cursor!=len(text):
+            cursor+=1
+    elif command[0]=='B':
+        if cursor!=0:
+            del(text[cursor-1])
+            cursor-=1
+    elif command[0]=='P':
+        text.insert(cursor,command[1])
+        cursor+=1
+
+print(''.join(text))
 """
