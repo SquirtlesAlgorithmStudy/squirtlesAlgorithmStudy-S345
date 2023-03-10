f569c181ecad617455cfd318959a48573d9a6dd8
gravity0717 <1357vvv@naver.com>
2023-03-10 18:58:43 +0900

    '0310'

---
+++ 찬호/11052.py
@@ -0,0 +1,9 @@
+N = int(input())
+p = [0] + list(map(int,input().split()))
+dp = [0 for _ in range(N+1)]
+
+
+for i in range(1,N+1):
+    for k in range(1,i+1):
+        dp[i] = max(dp[i], dp[i-k] + p[k])
+print(dp[i])
