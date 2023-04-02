c45690b86383cc0d52805639a47ce23864d699bc
Trilly <tttrillyyy@gmail.com>
2023-04-02 13:17:25 +0900

    SVBootCamp Chapter10-3 practice DoubliyLinkedList popAfter(),popBefore(),popAt()

---
+++ SangminByeon/SVBootCamp/10강 실습: (2) 양방향 연결 리스트 노드 삽입.py
@@ -0,0 +1,43 @@
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.prev = None
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.tail.next = None
+
+
+    def traverse(self):
+        result = []
+        curr = self.head
+        while curr.next.next:
+            curr = curr.next
+            result.append(curr.data)
+        return result
+
+
+    def insertBefore(self, next, newNode):
+        if next is None or next is self.head:
+            return False
+        prev = next.prev
+        newNode.next = next
+        newNode.prev = prev
+        next.prev = newNode
+        prev.next = newNode
+        self.nodeCount += 1
+        return True
+
+
+def solution(x):
+    return 0
