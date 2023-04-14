a19bc5ab4398abca52fdea50dfc05e5ed5cfc902
Trilly <tttrillyyy@gmail.com>
2023-04-14 10:47:57 +0900

    HackerLink 3MonPrepKit Week1-4

---
+++ SangminByeon/HackerLink/Breaking the Records.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    # Write your code here
+    minimum, maximum = scores[0],scores[0]
+    min_cnt, max_cnt = 0,0
+    
+    for score in scores:
+        if score < minimum:
+            minimum = score
+            min_cnt +=1
+        elif score > maximum:
+            maximum = score
+            max_cnt +=1
+    
+    return [max_cnt,min_cnt]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
--- SangminByeon/HackerLink/TimeConversion.py
+++ SangminByeon/HackerLink/TimeConversion.py
@@ -1,2 +1,41 @@
 # https://www.hackerrank.com/challenges/three-month-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    time, noon = s[:-2].split(':'), s[-2:]
+    hour = int(s[:2])
+    
+    if noon == 'PM':
+        hour = hour%12 + 12
+    else:
+        hour = hour%12
+    time[0] = ''+str(hour) if hour//10 else '0'+str(hour)
+    
+    return ':'.join(time)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
